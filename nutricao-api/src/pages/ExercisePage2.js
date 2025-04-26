ab99bab7f5194c14a637f457cbdb844f55e19c04
Rodrigo Appelt <rappelt@inf.ufsm.br>
2025-04-26 17:00:44 -0300

    progress: adiciona capacidade de filtrar por categoria
    ainda nao aparece nada na tela mas faz parte

--- nutricao-api/src/pages/ExercisePage2.js
+++ nutricao-api/src/pages/ExercisePage2.js
@@ -23,27 +23,48 @@ const exercises = [
 ];
 
 function ExercisePage2() {
+    const imageNotFoundImage = (
+        <svg
+                    className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-1p9wzm4 exercise-placeholder"
+                    focusable="false"
+                    aria-hidden="true"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2M8.5 13.5l2.5 3.01L14.5 12l4.5 6H5z"></path>
+                  </svg>
+    )
     const [exercises, setExercises] = useState([]);
+    const [filteredExercises, setFilteredExercises] = useState([]);
     const [loading, setLoading] = useState(true);
     const [languageFilter, setLanguageFilter] = useState("");
     const [categoryFilter, setCategoryFilter] = useState("");
+    const [categories, setCategories] = useState([]);
 
     useEffect(() =>{
         async function fetchExercises() {
             const apiUrl = 'https://wger.de/api/v2/exerciseinfo/';
+            const apiCategoryUrl = 'https://wger.de/api/v2/exercisecategory/';
             const apiKey = process.env.REACT_APP_API_KEY;
         
             try {
-                const response = await fetch(apiUrl, {
+                let response = await fetch(apiUrl, {
                   headers: {
                     Authorization: `Token ${apiKey}`,
                   },
                 });
-                const data = await response.json();
-                // const filteredExercises = data.results.filter((exercise) =>
-                //    exercise.translations.some((translation) => translation.language === 2)
-                // );
+                let data = await response.json();
                 setExercises(data.results);
+
+                // procura as categorias
+                response = await fetch(apiCategoryUrl, {
+                    headers: {
+                        Authorization: `Token ${apiKey}`,
+                    }
+                });
+                data = await response.json();
+                setCategories(data.results.map((category) => {
+                    return category.name
+                }));
               } catch (error) {
                 console.error('Error searching for exercises', error);
                 return [];
@@ -52,11 +73,16 @@ function ExercisePage2() {
               }
         }
         fetchExercises()
+        setFilteredExercises(exercises);
         setLoading(false);
     }, []);
 
-    const filteredExercises = exercises.filter((exercise) => {
-        return categoryFilter ? exercise.category === categoryFilter : true;
+    let effectiveExercises = exercises.filter((exercise) => {
+        console.log(`Exercicio ${exercise.id}: category: ${exercise.category}, language: ${exercise.translations}. CurrentFilter: ${categoryFilter}`);
+        return categoryFilter ? exercise.category.name === categoryFilter : true;
+    });
+    effectiveExercises = effectiveExercises.filter((exercise) => {
+        return languageFilter ? exercise.translations.some((translation) => translation.language === parseInt(languageFilter)) : true;
     });
 
     return (
@@ -72,7 +98,7 @@ function ExercisePage2() {
                     </div>
                 ) : (
                     <div style={{ padding: '24px', textAlign: 'center' }}>
-                        <p style={{ fontSize: '18px' }}>Total de Exercícios: {filteredExercises.length}</p>
+                        <p style={{ fontSize: '18px' }}>Total de Exercícios: {effectiveExercises.length}</p>
                     </div>
                 )
             }
@@ -84,10 +110,6 @@ function ExercisePage2() {
                     onChange={
                         (e) => {
                             setLanguageFilter(e.target.value)
-                            const filteredExercises = exercises.filter((exercise) =>
-                                exercise.translations.some((translation) => translation.language === 2)
-                            );
-                            setExercises(filteredExercises);
                         }
                     }
                 >
@@ -99,29 +121,43 @@ function ExercisePage2() {
                 <select
                     style={{ border: '1px solid #ccc', padding: '8px', borderRadius: '4px' }}
                     value={categoryFilter}
-                    onChange={(e) => setCategoryFilter(e.target.value)}
+                    onChange={(e) => {
+                        setCategoryFilter(e.target.value)
+                    }}
                 >
                     <option value="">Todas as Categorias</option>
-                    <option value="Lógica">Lógica</option>
-                    <option value="Algoritmos">Algoritmos</option>
-                    <option value="Estruturas de Dados">Estruturas de Dados</option>
+                    {
+                        categories.map((category) => {
+                            return (
+                                <option key={category} value={category}>{category}</option>
+                            )
+                        })
+                    }
                 </select>
             </div>
 
             <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))', gap: '24px', padding: '16px' }}>
-                {filteredExercises.map((exercise) => (
-                    <div key={exercise.id} style={{ border: '1px solid #ccc', borderRadius: '8px', overflow: 'hidden', display: 'flex', flexDirection: 'column', height: '320px', textAlign: 'center' }}>
+                {effectiveExercises.map((exercise) => {
+                    const englishTranslation = exercise.translations.find(
+                        (translation) => translation.language === 2
+                    );
+                    const name = englishTranslation?.name || 'Name not available';
+                    const categories = exercise.category.name || 'Category not available';
+                    const image = exercise.images.length > 0 ? exercise.images[0].image : imageNotFoundImage;
+                    
+                    (
+                    <div key={name} style={{ border: '1px solid #ccc', borderRadius: '8px', overflow: 'hidden', display: 'flex', flexDirection: 'column', height: '320px', textAlign: 'center' }}>
                         <img
-                            src={exercise.image}
-                            alt={exercise.title}
+                            src={image}
+                            alt={name}
                             style={{ width: '100%', height: '160px', objectFit: 'cover' }}
                         />
                         <div style={{ padding: '16px', flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-                            <h3 style={{ fontSize: '20px', margin: '8px 0' }}>{exercise.title}</h3>
-                            <p style={{ fontSize: '14px', color: '#777' }}>{exercise.category}</p>
+                            <h3 style={{ fontSize: '20px', margin: '8px 0' }}>{name}</h3>
+                            <p style={{ fontSize: '14px', color: '#777' }}>{categories}</p>
                         </div>
                     </div>
-                ))}
+                )})}
             </div>
 
             <div style={{ display: 'flex', justifyContent: 'center', gap: '16px', padding: '24px' }}>
